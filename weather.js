c683eca268b2616534653f29731a8d65c76bfa48
Rahul <77574839+visrm@users.noreply.github.com>
2024-02-27 02:25:45 +0530

    🎨 weather.js

--- weather.js
+++ weather.js
@@ -12,26 +12,26 @@ const searchbtn = document.querySelector("button");
 const searchbar = document.querySelector(".search-bar");
 
 const weather = {
-    fetchWeatherAPI: function (city) {
-        fetch(`"https://weather-api99.p.rapidapi.com/weather?city=${city}"`, options)
+  fetchWeatherAPI: function (city) {
+        fetch(`https://weather-api99.p.rapidapi.com/weather?city=${city}`, options)
         .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
         .catch((err) => console.log(err));
-        searchbar.value = " ";
     },
     displayWeather: function (data) {
         const { name } = data;
-        const { icon, desciption } = data.weather[0];
+        const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         document.querySelector(".city").innerText = name;
         document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-        document.querySelector(".description").innerText = desciption;
+        document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity :" + humidity + "%";
         document.querySelector(".wind").innerText = "Wind-speed :" + speed + "km/h";
     },
     search: function () {
-        this.fetchWeatherAPI(searchbar.value)
+        this.fetchWeatherAPI(searchbar.value.trim())
     }
 };
 
